ccead3b76b41a0d7e65012945778bb2591d6ca47
gizem2410 <eaagica@students.eaaa.dkgit config --global user.email eaagica@students.eaaa.dk>
2021-11-01 10:20:43 +0100

    Ny

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,5 @@
 var slideIndex = 0;
 showSlides();
-
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
@@ -17,3 +16,95 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+  //Videoer og mute ikoner gemt i array
+  let ourVideos = [
+    document.getElementById('tekstVideo'),
+    document.getElementById('pictureVideo')
+];
+
+let muteIcon = [
+    '<img src="img/soundOn.svg" alt="sound icon">',
+    '<img src="img/soundOff.svg" alt="sound icon">'
+]
+
+//Knap function til at pause og spille stemnings videoen med tekst og lyd
+$('#topPauseButton').click(function(){
+    if (ourVideos[0].paused) {
+        ourVideos[0].play();
+        $('#topPauseButton').html(muteIcon[0]);
+    } else {
+        ourVideos[0].pause();
+        $('#topPauseButton').html(muteIcon[1]);
+    }
+});
+
+//Function til at afspille lydinterview når man hover over billederne, og pause dem når man ikke gør
+let student = [
+    '#arthur',
+    '#amy',
+    '#sofus'
+];
+
+let soundInterview = [
+    document.getElementById('arthurSound'),
+    document.getElementById('amySound'),
+    document.getElementById('sofusSound')
+];
+
+let studentPic = [
+    $('#arthurPic'),
+    $('#amyPic'),
+    $('#sofusPic')
+];
+
+function picAnimation (picToAnimate) {
+    picToAnimate.css({
+        "width": "100%",
+        "transform": "rotate(5deg)",
+        "transition-duration": "1s"
+    });
+}
+
+function resetPic (picToReset) {
+    picToReset.css({
+        "width": "90%",
+        "transform": "rotate(0deg)",
+        "transition-duration": "1s"
+    });
+}
+
+//Arthur interview
+$(student[0]).mouseover(function(){
+    soundInterview[0].play();
+    picAnimation(studentPic[0]);
+});
+$(student[0]).mouseout(function(){
+    soundInterview[0].pause();
+    resetPic(studentPic[0]);
+});
+
+//Amy interview
+$(student[1]).mouseover(function(){
+    soundInterview[1].play();
+    picAnimation(studentPic[1]);
+});
+$(student[1]).mouseout(function(){
+    soundInterview[1].pause();
+    resetPic(studentPic[1]);
+});
+
+//Sofus interview
+$(student[2]).mouseover(function(){
+    soundInterview[2].play();
+    picAnimation(studentPic[2]);
+});
+$(student[2]).mouseout(function(){
+    soundInterview[2].pause();
+    resetPic(studentPic[2]);
+});
+
+
+
+// ... din kode slut ...
+
+// denne line må ikke slettes
